bec503f6e494681f9a7e99108a7d68647ad0f823
pr45h <prashantkkhanal@gmail.com>
2024-06-02 15:11:08 +0545

    initial

---
+++ src/utils/resume.js
@@ -0,0 +1,17 @@
+const myResume =()=>{
+    const resumeBtn= document.querySelector('.header__resume');
+
+
+resumeBtn.addEventListener('click',()=>{
+    // var event;
+    // event.preventDefault();
+    // console.log("is ok");
+    // if(confirm("Do you want to download?")){
+    window.location.href="./files/re.pdf";
+
+
+       
+        // console.log(isMobileNavOpen);
+    });
+}
+export default myResume;
